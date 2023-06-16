0a6fb70f3a822bec1f8cc51b312b27aa0769e9cb
Urbano <urbanobaz@yahoo.com>
2023-06-16 14:46:26 -0400

    More progress

---
+++ components/bookCard/BookCard.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Link from "next/link";
+import styles from "./BookCard.module.css";
+
+type BookCardProps = {
+  title: string;
+  author: string;
+  pages: number;
+  publishedDate: string;
+  id: string;
+  read: boolean;
+};
+
+const BookCard: React.FC<BookCardProps> = ({ title, author, pages, read }) => {
+  return (
+    <Link href="/">
+      <div className={styles.card}>
+        <div className={styles.cardContents}>
+          <h3>{title}</h3>
+          <p>by {author}</p>
+          <p>Pages: {pages}</p>
+          <div className={styles.row}>
+            <p>Read:</p>
+            <div className={styles.checkboxWrapper}>
+              <input type="checkbox" checked={read} />
+            </div>
+          </div>
+          <button className={styles.button} type="button">
+            Delete
+          </button>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default BookCard;
