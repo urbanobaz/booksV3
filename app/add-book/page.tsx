abcd310b09939a341bc634de5d2a582ca7f2360d
Urbano <urbanobaz@yahoo.com>
2023-06-24 17:46:25 -0400

    Centering sign in page and creating css file for add book page

--- app/add-book/page.tsx
+++ app/add-book/page.tsx
@@ -3,11 +3,12 @@
 import AddBookForm from "@/components/addBookForm/AddBookForm";
 import { currentUser } from "@clerk/nextjs";
 import React from "react";
+import { cn } from "@/app/helpers/utils";
 
 export default async function AddBookPage() {
   const user = await currentUser();
   return (
-    <div className="h-full flex justify-center">
+    <div className={cn("h-full flex justify-center")}>
       <AddBookForm
         email={
           user?.emailAddresses[0].emailAddress
