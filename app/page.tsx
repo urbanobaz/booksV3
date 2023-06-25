a0c085287063e77fb3a9e7339ce8bcff0d81dec1
Urbano <urbanobaz@yahoo.com>
2023-06-25 13:25:13 -0400

    More UI edits, responsiveness edits

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="container max-w-7xl mx-auto my-auto w-full h-full flex flex-col justify-center">
+    <div className="home-wrapper">
       <h1 className="main-home-text">
         Sign up or sign in to add books to your list!
       </h1>
